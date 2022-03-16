789107a7f77abc4dff93b0a4db0d007f752d05a8
Jhon <Jhonatanmedina1590@gmail.com>
2022-03-15 18:27:22 -0700

    touched up on sign in page and worked out the redirection
    Signed-off-by: Jhon <Jhonatanmedina1590@gmail.com>

--- app/src/main/java/com/example/moodtracker/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/moodtracker/ui/login/LoginActivity.java
@@ -1,64 +1,66 @@
 package com.example.moodtracker.ui.login;
 
-import android.app.Activity;
-
 import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.util.Patterns;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.moodtracker.MainActivity;
 import com.example.moodtracker.R;
 import com.example.moodtracker.RegisterUser;
-import com.example.moodtracker.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-
+    /** ****** variables used for the interface ******
+     * Button for to move to register
+     * Entry fields
+    */
     private TextView register;
     private EditText editTextEmail, editTextPasswords;
 
+    // Button used to send the sign in request
     private Button signInButton;
+
+    // For use with firebase
     private FirebaseAuth mAuth;
 
-
-
+    /*** initializes variables */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // ****** Calling the UI that first displays on start up ******
         super.onCreate(savedInstanceState);
+            // We will use call the login UI
         setContentView(R.layout.activity_login);
 
-        register = (Button) findViewById(R.id.register_from_login1);
+        // Initialize button to link with ID
+        register = findViewById(R.id.register_from_login1);
         register.setOnClickListener(this);
 
         // Sign ib button
-        signInButton = (Button) findViewById(R.id.login_login_button);
+        signInButton = findViewById(R.id.login_login_button);
         signInButton.setOnClickListener(this);
 
-        editTextEmail =(EditText) findViewById(R.id.login_username_field);
-        editTextPasswords = (EditText) findViewById(R.id.login_password_field);
+        //  fields to enter email and password
+        editTextEmail = findViewById(R.id.login_username_field);
+        editTextPasswords = findViewById(R.id.login_password_field);
+
+        // initialize firebase instance
+        mAuth = FirebaseAuth.getInstance();
     }
 
+//    ****** switch to respond to different buttons ******
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -66,30 +68,58 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(new Intent(this, RegisterUser.class));
                 break;
             case R.id.login_login_button:
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-//                userLogin();
+                userLogin();
                 break;
         }
-
     }
 
+    /** ****** Login Function  ******
+          This function will take care if the sign-in activities.
+          Will contact firebase and request to log-in.
+          Will pass email and password.
+      */
     private void userLogin() {
+        // variables get and trim the text from the input fields
+        // We trim to get rid of any extra white space at the beginning or end
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPasswords.getText().toString().trim();
 
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        // Checking the entry fields for compliance.
+        if(email.isEmpty()){
+            editTextEmail.setError("Please enter valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPasswords.setError("Enter a valid password");
+            editTextPasswords.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPasswords.setError("Enter a valid password");
+            editTextPasswords.requestFocus();
+            return;
+        }
+        /** Actually calls the to firebase to make the request
+         *  If the user signs-in correctly they will be redirected to the main page
+         *  If the sign in is not successful an error will be provided
+         *  */
+        this.mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    // redirect user
+                if (task.isSuccessful()) {
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                } else {
+                    // Toast lets user know their information is not orrect
+                    Toast.makeText(LoginActivity.this, "Failed to sign-in. Email or password are incorrect.", Toast.LENGTH_LONG).show();
                 }
-                else{
-                    Toast.makeText(LoginActivity.this, "Failed to login",Toast.LENGTH_LONG).show();
-
-
-                }            }
+            }
         });
-
     }
+
 }
