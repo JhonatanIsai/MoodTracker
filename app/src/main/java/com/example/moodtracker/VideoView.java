18514d020d037933078e979959c4200fbc397676
Jhon <Jhonatanmedina1590@gmail.com>
2022-04-06 23:01:08 -0700

    added youtube videos for toolbox. Still need to add rest of exercise videos.
    Signed-off-by: Jhon <Jhonatanmedina1590@gmail.com>

---
+++ app/src/main/java/com/example/moodtracker/VideoView.java
@@ -0,0 +1,50 @@
+package com.example.moodtracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.media.session.MediaController;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class VideoView extends AppCompatActivity {
+
+    private YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+
+        youTubePlayerView =findViewById(R.id.youtube_video);
+        getLifecycle().addObserver(youTubePlayerView);
+
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                super.onReady(youTubePlayer);
+                String videoId = "VSMAjMfJ6KQ";
+                youTubePlayer.loadVideo(videoId,0);
+
+            }
+
+
+        });
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/moodtracker/wellness_toolbox.java
+++ app/src/main/java/com/example/moodtracker/wellness_toolbox.java
@@ -2,13 +2,31 @@ package com.example.moodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class wellness_toolbox extends AppCompatActivity {
+public class wellness_toolbox extends AppCompatActivity implements View.OnClickListener{
 
+    View breathing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellness_toolbox);
+
+        breathing = findViewById(R.id.imageButton_breathe);
+        breathing.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.imageButton_breathe:
+                startActivity(new Intent(this, VideoView.class));
+                break;
+        }
     }
 }
